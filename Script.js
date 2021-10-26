296644787076c8b37ff183efcff363466ff24cce
Gustavo2A <91330308+Gustavo2A@users.noreply.github.com>
2021-10-26 15:41:17 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,32 @@
+// Hola
+// Valor xd
+
+console.log ('Hola consola')
+console.log (15 + 20)
+console.log (1000 - 578)
+console.log (90 * 100) 
+console.log (8 / 2)
+
+// la calificacion 1 es de 9 
+// la calificacion 2 es de 10
+// la calificacion 3 es de 7.5
+// por lo tanto el promedio es: 8.8333
+
+// recopilar las calificaciones
+var cali1 = 10, cali2  = 7, cali3 = 8.25;
+//operaciones
+var suma = cali1 + cali2 + cali3;
+const nCails = 3;
+var promedio = suma / nCails;
+
+// mostrar resultados
+console.log('La calificacion 1 es' + cali1);
+console.log('La calificacion 2 es' + cali2);
+console.log('La calificacion 3 es' + cali3);
+console.log('Por lo tanto el promedio es' + promedio);
+
+//fin
+
+/* 
+
+*/
